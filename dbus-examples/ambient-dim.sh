31c5c3822a2c70b9baf66daa7916bf61b6c56886
Dimitrios Semitsoglou-Tsiapos <kmhzsem@gmx.com>
2017-05-06 23:10:23 +0300

    ambient-dim: fixes

--- dbus-examples/ambient-dim.sh
+++ dbus-examples/ambient-dim.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# === Verify `compton --dbus` status ===
-
-if [ -z "`dbus-send --session --dest=org.freedesktop.DBus --type=method_call --print-reply /org/freedesktop/DBus org.freedesktop.DBus.ListNames | grep compton`" ]; then
-    echo "compton DBus interface unavailable"
-    if [ -n "`pgrep compton`" ]; then
-        echo "compton running without dbus interface"
-        #killall compton & # Causes all windows to flicker away and come back ugly.
-        #compton --dbus & # Causes all windows to flicker away and come back beautiful
-    else
-        echo "compton not running"
-    fi
-    exit 1;
-fi
-
 # === Get connection parameters ===
 
 dpy=$(echo -n "$DISPLAY" | tr -c '[:alnum:]' _)
+unbright="/dev/shm/unbright.$dpy"
 
 if [ -z "$dpy" ]; then
     echo "Cannot find display."
@@ -29,15 +16,6 @@ interface="com.github.chjj.compton"
 compton_dbus="dbus-send --print-reply=literal --dest="${service}" / "${interface}"."
 compton_dbus_h="dbus-send --print-reply --dest="${service}" / "${interface}"."
 
-# === Try using flock ===
-
-unbright="/dev/shm/unbright.$dpy"
-if hash flock 2>/dev/null; then
-    flock="flock ${unbright}"
-else
-    echo 'flock not found, running without it'
-fi
-
 # === Get camera device ===
 if [[ -z "$1" ]] || [[ ! -c "$1" ]]; then
     echo "usage: $0 <camera device>"
@@ -48,23 +26,23 @@ fi
 
 # === Sample parameters ===
 
-max_dim=55
+max_dim=65
 frame_ms=30
-rule='!(class_g ~= "^(URxvt|mpv|Sxiv)$" || _NET_WM_WINDOW_TYPE@:32a *= "DOCK")'
+rule='!(class_g ~= "^(URxvt|mpv|Sxiv|syncterm|XTerm|stellarium|XEphem)$" || _NET_WM_WINDOW_TYPE@:32a *= "DOCK")'
 
 # === Trap ===
 
-trap '_remove; _repaint; trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
+trap 'kill $(jobs -p); _remove; _repaint;' EXIT
 
 _insert() {
-    ${compton_dbus}dim_rule_update string:insert string:"${1}:${rule}"
+    ${compton_dbus}dim_rule_update string:insert string:"${1}:${rule}" 2>/dev/null
 }
 
 _remove() {
     if [ ! -z "$last_repr" ]; then
         ${compton_dbus}dim_rule_update string:remove string:"$last_repr" >/dev/null 2>&1
         if [[ $? -ne 0 ]]; then
-            # Exiting during replace(). Fingers crossed we're removing the
+            # Exiting during set_dim(). Fingers crossed we're removing the
             # right thing
             last_repr=$(${compton_dbus_h}opts_get string:dim-rule | \
                 grep -m1 -Po '(?<=string ").*(?=")')
@@ -81,7 +59,7 @@ _repaint() {
     ${compton_dbus}repaint >/dev/null
 }
 
-replace() {
+set_dim() {
     local new
     if [[ -z "$last_repr" ]]; then
         new=$(_insert $1)
@@ -89,34 +67,42 @@ replace() {
         new=$(_replace $1)
     fi
     last_repr=${new:3}
+    last_dim="$1"
     _repaint
 }
 
 _normalize() {
-    old_min=72
-    old_max=184
-    new_min=0
-    new_max=100
-    old_val=$1
+    old_min=$1
+    old_max=$2
+    new_min=$3
+    new_max=$4
+    old_val=$5
     new_val=$(( (((old_val - old_min) * (new_max - new_min)) / \
         (old_max - old_min)) + new_min ))
+    if [[ $new_min -gt $new_max ]]; then
+        [[ $new_val -gt $new_min ]] && new_val=$new_min
+        [[ $new_val -lt $new_max ]] && new_val=$new_max
+    fi
     echo $new_val
 }
 
 read_camera() {
-    while read -r brightness; do
-        $flock echo $(_normalize "$brightness") > $unbright
-    done < <(ffmpeg \
+    exec ffmpeg \
         -hide_banner \
         -f video4linux2 \
         -s 640x480 \
         -i "$cam" \
         -filter:v "fps=fps=30, showinfo" \
-        -f null - 2>&1 \
-        | stdbuf -oL grep -Po '(?<=mean:\[)[0-9]*')
+        -f null - > >(stdbuf -oL grep -Po '(?<=mean:\[)[0-9]*' | \
+        while read -r brightness; do
+            echo $(_normalize 72 184 0 100 "$brightness") > "${unbright}_"
+            mv "${unbright}_" "${unbright}"
+        done) 2>&1
 }
 
 init_camera() {
+    [[ ! -c $cam ]] && exit 1
+
     # Try changing this if you don't get good results
     v4l2-ctl -d "$cam" -c exposure_auto=3 2>/dev/null
     v4l2-ctl -d "$cam" -c exposure_auto=1 2>/dev/null
@@ -130,38 +116,55 @@ init_camera() {
     else
         v4l2-ctl -d "$cam" -c exposure_absolute="$expo" 2>/dev/null
     fi
-
 }
 
-rm -f "$unbright"
+set_ddc() {
+    local new="$1"
+    if [[ "$new" -ne "$last_ddc" ]]; then
+        if ! ps -p $ddc_pid >/dev/null 2>&1; then
+            ddccontrol -r 0x10 -w "$new" dev:/dev/i2c-2 >/dev/null 2>&1 &
+            ddc_pid=$!
+            last_ddc="$new"
+        fi
+    fi
+}
+
+dim_to() {
+    local new_aim="$1"
+    if [[ $new_aim -gt $(( last_dim + 1 )) ]]; then
+        set_dim $(( last_dim + 1 ))
+    elif [[ $new_aim -lt $(( $last_dim - 1 )) ]]; then
+        set_dim $(( last_dim - 1 ))
+    fi
+}
+
+echo > "$unbright"
 init_camera
 read_camera &
 
-last_set=0
-last_brightness=0
+last_dim=0
+last_ddc=0
 while :; do
     # TODO Implement frame-dropping
     start_date=$(date +%s%3N)
 
-    brightness=$($flock cat "$unbright" 2>/dev/null)
+    brightness=$(<"$unbright")
     if [[ ! -z $brightness ]]; then
 
-        new_aim=$(( $max_dim - $brightness ))
-        [[ $new_aim -lt 0 ]] && new_aim=0
-        [[ $new_aim -gt 100 ]] && new_aim=100
+        # DIM
+        dim_aim=$(_normalize 0 85 65 0 $brightness)
+        dim_to "$dim_aim"
 
-        # +1 is poor man's way of avoiding some flicker. TODO become rich
-        if [[ $new_aim -gt $(( last_set + 1 )) ]]; then
-            last_set=$(( last_set + 1 ))
-            replace $last_set
-        elif [[ $new_aim -lt $(( $last_set - 1 )) ]]; then
-            last_set=$(( last_set - 1 ))
-            replace $last_set
+        # DDC
+        if [[ $brightness -gt 50 ]]; then
+            new_ddc=$(_normalize 50 100 20 100 $brightness)
+        else
+            new_ddc=20
         fi
-        # echo $brightness $new_aim $last_set
-    fi
+        set_ddc $new_ddc
 
-    last_brightness=$brightness
+        # echo "$brightness -> ($new_ddc, $dim_aim)"
+    fi
 
     end_date=$(date +%s%3N)
     diff_date=$(( end_date - start_date ))
