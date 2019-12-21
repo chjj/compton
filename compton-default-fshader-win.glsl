92a7e3feeed139793a557f2b04f3b9b9eb5ad3ac
Tobia <tobia.conforto@gruppo4.it>
2019-12-21 18:11:41 +0100

    Updated GLSL shader example

--- compton-default-fshader-win.glsl
+++ compton-default-fshader-win.glsl
@@ -1,11 +1,24 @@
 uniform float opacity;
 uniform bool invert_color;
 uniform sampler2D tex;
+uniform vec4 margin;
+
+float min3(vec3 v) { return min(min(v.x, v.y), v.z); }
+float max3(vec3 v) { return max(max(v.x, v.y), v.z); }
 
 void main() {
-	vec4 c = texture2D(tex, gl_TexCoord[0]);
-	if (invert_color)
-		c = vec4(vec3(c.a, c.a, c.a) - vec3(c), c.a);
-	c *= opacity;
-	gl_FragColor = c;
+    vec2 coord = vec2(gl_TexCoord[0]);
+    vec4 c = texture2D(tex, coord);
+
+    // If current window should be inverted and the pixel is inside client area
+    if (invert_color
+        && all(greaterThan(coord.xy, margin.st))
+        && all(lessThan(coord.xy, margin.pq))
+    ) {
+        // Fast luminance inversion, while preserving hue
+        c.rgb += 1.0 - max3(c.rgb) - min3(c.rgb);
+    }
+
+    c *= opacity;
+    gl_FragColor = c;
 }
